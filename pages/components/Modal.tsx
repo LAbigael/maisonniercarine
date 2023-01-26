83c973f914ab4aecd815be170e9f0e6a058a4a9a
Abigael Laldji <abigael.laldji@protonmail.com>
2023-01-26 12:01:15 +0100

    add files

---
+++ pages/components/Modal.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+type props = {
+  text: string;
+  button: string;
+};
+const App = ({ text, button }: props) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
+
+  return (
+    <>
+      <div>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="save-button text-gray-600"
+          onClick={() => (modalOpen ? close() : open())}
+        >
+          {button}
+        </motion.button>
+      </div>
+      <AnimatePresence
+        // Disable any initial animations on children that
+        // are present when the component is first rendered
+        initial={false}
+        // Only render one component at a time.
+        // The exiting component will finish its exit
+        // animation before entering component is rendered
+        wait={true}
+        // Fires when all exiting nodes have completed animating out
+        onExitComplete={() => null}
+      >
+        {modalOpen && (
+          <Modal text={text} modalOpen={modalOpen} handleClose={close} />
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+
+const Backdrop = ({ children, onClick }) => {
+  return (
+    <motion.div
+      onClick={onClick}
+      className="backdrop"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
+const Modal = ({ handleClose, text }) => {
+  return (
+    <Backdrop onClick={handleClose}>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}
+        className="modal orange-gradient"
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        <p dangerouslySetInnerHTML={{ __html: text }} />
+        <button onClick={handleClose}>Close</button>
+      </motion.div>
+    </Backdrop>
+  );
+};
+
+export default App;
