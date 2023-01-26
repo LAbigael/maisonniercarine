64d6ad301e93634346942cfe048ad01f10f3a335
Abigael Laldji <abigael.laldji@protonmail.com>
2023-01-26 11:51:22 +0100

    new design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        palette: {
+          yellow: "#FDFDBD",
+          green: "#C8FFD4",
+          blue: "#B8E8FC",
+          purple: "#B1AFFF",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
